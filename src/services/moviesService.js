16288bf55c5dd9906cd40226a791f2831e3cf9e2
Gaps <764590@nur.edu.bo>
2026-06-25 11:13:08 -0400

    Front lindo

---
+++ src/services/moviesService.js
@@ -0,0 +1,88 @@
+import axios from 'axios'
+import { API_URL } from '../utils/constants'
+
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+})
+
+const moviesService = {
+  getAll: (filtros = {}) => {
+    return new Promise((resolve, reject) => {
+      api.get('/peliculas', { params: filtros })
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+
+  getById: (id) => {
+    return new Promise((resolve, reject) => {
+      api.get(`/peliculas/${id}`)
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+
+  // datos: { titulo, sinopsis, genero, duracion, clasificacion, poster (File) }
+  create: (datos) => {
+    return new Promise((resolve, reject) => {
+      const formData = construirFormData(datos)
+      api.post('/peliculas', formData)
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+
+  update: (id, datos) => {
+    return new Promise((resolve, reject) => {
+      const formData = construirFormData(datos)
+      api.put(`/peliculas/${id}`, formData)
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+
+  remove: (id) => {
+    return new Promise((resolve, reject) => {
+      api.delete(`/peliculas/${id}`)
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+}
+
+// El backend espera multipart/form-data con el archivo bajo la key 'poster'
+const construirFormData = (datos) => {
+  const formData = new FormData()
+  formData.append('titulo', datos.titulo)
+  formData.append('sinopsis', datos.sinopsis)
+  formData.append('genero', datos.genero)
+  formData.append('duracion', datos.duracion)
+  formData.append('clasificacion', datos.clasificacion)
+  if (datos.poster) {
+    formData.append('poster', datos.poster)
+  }
+  return formData
+}
+
+export default moviesService
